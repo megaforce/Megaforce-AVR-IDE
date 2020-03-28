f54702ec62bb9fe5cfefcb107da1446def718686
megaforce <matjaz.zibert2@gmail.com>
2020-03-28 12:49:46 +0100

    Created MegaforceAVR library. Added comments to MegaforceAVR library

---
+++ Code/MegaforceAVR.py
@@ -0,0 +1,48 @@
+import os
+def Compile():
+    CPU = "atmega328pb"
+    CompileString = ""
+    FILENAMES = []
+    COMPILEFILES = [];
+    FILELOCATIONS = ['/home/patricija/Namizje/AVR/PortManipulation/PortManipulation/main.c',
+                     '/home/patricija/Namizje/AVR/PortManipulation/PortManipulation/MMINIT.c',
+                     '/home/patricija/Namizje/AVR/PortManipulation/PortManipulation/systime.c']
+
+    PROJECTLOCATION = "";
+    for i in range (0,len(FILELOCATIONS)):
+        FILENAMES.append((os.path.basename(FILELOCATIONS[i])))
+
+    PROJECTLOCATION = os.path.abspath(FILELOCATIONS[0])
+    PROJECTLOCATION = PROJECTLOCATION.replace(FILENAMES[0],'project.elf')
+
+    for i in range(0,len(FILELOCATIONS)):
+        tmp = FILELOCATIONS[i]
+        tmp = tmp[:-1] + 'o'
+        COMPILEFILES.append(tmp)
+
+    for i in range(0,len(FILELOCATIONS)):
+        CompileString = 'avr-gcc -c -mmcu=' + CPU + ' -o '  + COMPILEFILES[i] + ' ' + FILELOCATIONS[i]
+        os.system(CompileString)
+
+    LinkString = "avr-gcc -mmcu=" + CPU + " -o "
+
+    for i in range(0,len(FILELOCATIONS)):
+        LinkString += COMPILEFILES[len(FILELOCATIONS)- 1 - i] + " "
+
+    LinkString += " -o " + PROJECTLOCATION
+
+
+    HexString = "avr-objcopy -O ihex -j .text -j .data " + PROJECTLOCATION + " " +  PROJECTLOCATION.replace('project.elf','project.hex')
+
+    os.system(LinkString)
+    os.system(HexString)
+def Upload():
+    CPU = "m328pb"
+    PROGTYPE = "arduino"
+    PROGLOCATION = "/dev/ttyUSB0"
+    BAUD = "115200"
+    PROJECTLOCATION = "/home/patricija/Namizje/AVR/PortManipulation/PortManipulation/project.hex"
+
+    UploadString = "sudo avrdude -p " + CPU + " -c " + PROGTYPE + " -P " + PROGLOCATION + " -b " + BAUD + ' -U flash:w:' + PROJECTLOCATION
+    print(UploadString)
+    os.system(UploadString)
