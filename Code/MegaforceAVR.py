d4a99c5aa711ae57e86df770adeaeaa221aafa53
megaforce <matjaz.zibert2@gmail.com>
2020-03-31 13:48:50 +0200

    Changed os script execution to Subprocess execution

--- Code/MegaforceAVR.py
+++ Code/MegaforceAVR.py
@@ -1,4 +1,5 @@
-import os
+import subprocess
+import shlex
 def Compile(CPU,FILELOCATIONS):
     # These are the files needed to store vital information for the parsing of the compile command
     CompileString = ""
@@ -21,7 +22,11 @@ def Compile(CPU,FILELOCATIONS):
 
     for i in range(0,len(FILELOCATIONS)):
         CompileString = 'avr-gcc -c -mmcu=' + CPU + ' -o '  + COMPILEFILES[i] + ' ' + FILELOCATIONS[i] # Generates the compile command for the current file
-        os.system(CompileString) # Runs the compile command
+        proc = subprocess.Popen(shlex.split(CompileString), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+        out, err = proc.communicate()# Runs the compile command
+        print(out)
+        print(err)
+
 
     LinkString = "avr-gcc -mmcu=" + CPU + " -o " # Header of the link command
 
@@ -34,9 +39,15 @@ def Compile(CPU,FILELOCATIONS):
     HexString = "avr-objcopy -O ihex -j .text -j .data " + PROJECTLOCATION + " " + PROJECTLOCATION.replace('project.elf','project.hex')
 
     # Runs the link command
-    os.system(LinkString)
+    proc = subprocess.Popen(shlex.split(LinkString), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = proc.communicate()
+    print(out)
+    print(err)
     # Runs the convert command
-    os.system(HexString)
+    proc = subprocess.Popen(shlex.split(HexString), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = proc.communicate()
+    print(out)
+    print(err)
 
 
 def Upload(CPUTYPE,PROGTYPE,PROGLOCATION,BAUD,PROJECTLOCATION):
@@ -44,9 +55,16 @@ def Upload(CPUTYPE,PROGTYPE,PROGLOCATION,BAUD,PROJECTLOCATION):
     # It has to be run with sudo otherwise the command might not be permited to execute
     UploadString = "sudo avrdude -p " + CPUTYPE + " -c " + PROGTYPE + " -P " + PROGLOCATION + " -b " + BAUD + ' -U flash:w:' + PROJECTLOCATION
     # Runs the upload script
-    os.system(UploadString)
+    proc = subprocess.Popen(shlex.split(UploadString), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = proc.communicate()
+    print(out)
+    print(err)
+
 
 def TestBoard(CPUTYPE,PROGTYPE,PROGLOCATION,BAUD):
     TestString = "sudo avrdude -p " + CPUTYPE + " -c " + PROGTYPE + " -P " + PROGLOCATION + " -b " + BAUD
     # Runs the upload script
-    os.system(TestString)
+    proc = subprocess.Popen(shlex.split(TestString), stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = proc.communicate()
+    print(out)
+    print(err)
